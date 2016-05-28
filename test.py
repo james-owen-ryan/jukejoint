29c7ca2d1fa44271ae42c0c6823ff2e2ec18c4ca
Tyler Brothers <tylerbrothers1@gmail.com>
2016-05-27 18:23:17 -0700

    Improved debug, No A.S., game loop improved
    * Removed action selectors so they can be replaced by symbol weights.
    * Debugging: productionist, skip bar, people and jukebox descriptions.
    * Core game loop improved to allow another song to be played after one
    finishes.

--- test.py
+++ test.py
@@ -5,17 +5,11 @@ sys.path.append(PATH_TO_ANYTOWN)
 # Now import from that Talk of the Town repository
 from game import Game
 import random
-from actionselector import Departure
 from song import Song, Stanza, Songs
 from business import Bar
 
 """GLOBAL VARS"""
 DEBUG = True if raw_input('ENGAGE DEBUG MODE? ') in ('yes', 'y', 'yeah', 'ok', 'sure', 'lol yep') else False
-
-ACTION_SELECTORS = [
-  Departure(scale=(-5,0,5))
-]
-
 SONGS = Songs
 
 """ FUNCTIONS """
@@ -36,13 +30,6 @@ def get_applicable_actionselectors(person, action_selectors):
     if action_selector.precondition(person):
       possible_selectors.append(action_selector)
   return possible_selectors
-
-def get_people_with_actionselectors(people):
-  action_people = []
-  for person in people:
-    if person.salient_action_selector is not None:
-      action_people.append(person)
-  return action_people
 
 def display_title_screen():
     """Display a title screen for the command-line version of this demo."""
@@ -86,6 +73,8 @@ while len(game.city.businesses_of_type('Bar')) < 3:
   owner = game._determine_who_will_establish_new_business(business_type=Bar)
   Bar(owner=owner)
 
+game.thought_productionist.debug = DEBUG
+
 #Rig the city to have bars with 1,2 and 3 people, respectively.
 num_people_in_bar = 1
 for bar in game.city.businesses_of_type('Bar'):
@@ -94,8 +83,6 @@ for bar in game.city.businesses_of_type('Bar'):
   while len(bar.people_here_now) < num_people_in_bar:
     game.random_person.go_to(bar)
   num_people_in_bar += 1
-
-game.thought_productionist.debug = DEBUG
 
 # Display demo title (unless debug mode is engaged)
 if not DEBUG:
@@ -109,61 +96,56 @@ display_bar_to_enter(bars)
 selection_index = int(raw_input('Select a bar to enter: '))
 chosen_bar = bars[selection_index]
 
-#Give people in the bar action selectors.
-for person in chosen_bar.people_here_now:
-  possible_selectors = get_applicable_actionselectors(person, ACTION_SELECTORS)
-  try:
-    person.salient_action_selector = random.choice(possible_selectors)
-  except IndexError: #no possible selectors are available to this person.
-    person.salient_action_selector = None
-  if DEBUG:
-    print '{} is debating about {}'.format(person.full_name, person.salient_action_selector)
-
 #Introduce the characters and the setting.
-display_intro_text()
+if not DEBUG: display_intro_text()
 
 #Introduce instructions on how to play.
-display_jukebox_instructions()
+if not DEBUG: display_jukebox_instructions()
 
-#Display the songs available to the user.
-display_songs()
+has_finished = False
+while not has_finished:
+  #Display the songs available to the user.
+  display_songs()
 
-#Get the users choice of song.
-selection_index = int(raw_input('You chose: '))
-current_song = SONGS[selection_index]
+  #Get the users choice of song.
+  selection_index = int(raw_input('You chose: '))
+  current_song = SONGS[selection_index]
 
-#Inform user and NPCs that a new song is starting
-print "\n{} begins to play...\n".format(current_song.id)
+  #Inform user and NPCs that a new song is starting
+  print "\n{} begins to play...\n".format(current_song.id)
 
-#loop through song lyrics
-song_stanzas = enumerate(current_song)
-current_stanza = song_stanzas.next()[1]
-continue_song = True
-THINKER = None
-while continue_song:
-  print "{}\n".format(current_stanza.lyrics)
-  #have npcs consider the symbols attached to this lyric
-  for person in get_people_with_actionselectors(chosen_bar.people_here_now):
-    THINKER = person
-    stimuli = person.mind.associate(current_stanza)
-    thought = person.mind.elicit_thought(stimuli)
-    #TODO: add thought to person's train of thoughts.
-    if thought:
-        print "{person}: {thought} ({signals})".format(
-            person=person.full_name,
-            thought=thought.realize(),
-            signals=", ".join(
-                "{signal} ({weight})".format(signal=signal, weight=weight) for signal, weight in thought.signals.iteritems()
-            )
-        )
-        thought.execute()
-        person.mind.thoughts.append(thought)
-    else:
-        print "{person}: ...".format(person=person.full_name)
-  try:
-    current_stanza = song_stanzas.next()[1]
-    cont = raw_input("Continue? (yes/no): ")
-    if cont.lower() not in ("yes", 'y', 'ok', 'sure'): continue_song = False
-  except StopIteration:
-    continue_song = False
-    print "No more lyrics"
+  #Loop through song lyrics
+  song_stanzas = enumerate(current_song)
+  current_stanza = song_stanzas.next()[1]
+  continue_song = True
+  THINKER = None
+  while continue_song:
+    print "{}\n".format(current_stanza.lyrics)
+    #Everyone in the bar will consider the song lyrics.
+    for person in chosen_bar.people_here_now:
+      THINKER = person
+      stimuli = person.mind.associate(current_stanza)
+      thought = person.mind.elicit_thought(stimuli)
+      if thought:
+          print "{person}: {thought} ({signals})".format(
+              person=person.full_name,
+              thought=thought.realize(),
+              signals=", ".join(
+                  "{signal} ({weight})".format(signal=signal, weight=weight) for signal, weight in thought.signals.iteritems()
+              )
+          )
+          thought.execute()
+          person.mind.thoughts.append(thought)
+      else:
+          print "{person}: ...".format(person=person.full_name)
+    try:
+      current_stanza = song_stanzas.next()[1]
+      cont = raw_input("Continue? (yes/no): ")
+      if cont.lower() not in ("yes", 'y', 'ok', 'sure'): continue_song = False
+    except StopIteration:
+      #Song is over.
+      continue_song = False
+      print "No more lyrics"
+
+    if chosen_bar.people_here_now == 0:
+      has_finished = True # Game Over.
